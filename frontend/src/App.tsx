171e8599be4963e7b05bc2969b5ef5a7a60cd109
Russlyman <10534452+Russlyman@users.noreply.github.com>
2025-04-30 13:49:36 +0100

    Add delete button for events

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -9,6 +9,20 @@ function App() {
   const [selectedDate, setSelectedDate] = useState<Date>(new Date());
   const [selectedEvents, setSelectedEvents] = useState<EventType[]>([]);
 
+  const deleteEvent = async (eventId: number) => {
+    const response = await fetch(`http://localhost:5281/events/${eventId}`, {
+      method: 'DELETE',
+    });
+
+    // Check response
+    if (!response.ok) {
+      console.error('Failed to delete');
+      return;
+    }
+
+    setSelectedEvents(prev => prev.filter(e => e.id !== eventId));
+  };
+
   useEffect(() => {
     const getEvents = async () => {
       // ACK: toIsoString will break because it forces UTC timezone,
@@ -17,6 +31,13 @@ function App() {
       const date = selectedDate.toLocaleDateString('en-CA');
 
       const response = await fetch(`http://localhost:5281/events?date=${date}`);
+
+      // Check response
+      if (!response.ok) {
+        console.error('Failed to get');
+        return;
+      }
+
       const responseJson = await response.json();
 
       const responseFixDate: EventType[] = responseJson.map(
@@ -33,6 +54,12 @@ function App() {
     <div key={e.id}>
       <p>{e.title}</p>
       <p>{e.description}</p>
+      <button
+        className="px-5 py-3 bg-gray-400"
+        onClick={() => deleteEvent(e.id)}
+      >
+        Delete
+      </button>
     </div>
   ));
 
